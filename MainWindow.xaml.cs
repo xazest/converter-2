eceaab50867a84580ad5a13efb3641a973aacec9
admin228 <admin228@DESKTOP-NTL96GO>
2025-05-28 15:09:17 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Operations operations;
+        public MainWindow()
+        {
+            InitializeComponent();
+            operations = new Operations(this);
+
+        }
+
+       static public string FromOrTo = "";
+
+
+        private void from_Checked(object sender, RoutedEventArgs e)
+        {
+            FromOrTo = "from";
+            inputChanged(null, null);
+        }
+        private void to_Checked(object sender, RoutedEventArgs e)
+        {
+            FromOrTo = "to";
+            inputChanged(null, null);
+        }
+
+        private void SB_Checked(object sender, RoutedEventArgs e)
+        {
+            inputChanged(null, null);
+        }
+
+        private void inputChanged(object sender, TextChangedEventArgs e)
+        {
+            if (operations != null)
+            {
+                if (!operations.huesos)
+                    operations.MainOperation();
+                else
+                {
+                    operations.HuesosOrNo();
+                    if (!operations.huesos) operations.MainOperation();
+                }
+            }
+        }
+    }
+
+}
---
+++ Operations.cs
@@ -0,0 +1,86 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    class Operations
+    {
+        private MainWindow _mainWindow;
+
+        public Operations(MainWindow mainWindow) { _mainWindow = mainWindow; }
+
+
+        float amountOfMoney;
+        float result;
+        string action = "";
+        public bool huesos = true;
+
+        public void MainOperation()
+        {
+            if (float.TryParse(_mainWindow.inputTextBox.Text, out amountOfMoney) && amountOfMoney >= 0)
+            {
+                Calculation();
+                _mainWindow.outputTextBox.Text = $"{result}";
+            }
+            else
+            {
+                _mainWindow.outputTextBox.Text = "пошел нахуй";
+            }
+
+        }
+        void Calculation()
+        {
+            FindCurrency();
+            SellOrBuy();
+            if (MainWindow.FromOrTo == "from")
+            {
+                result = amountOfMoney * Exchange.GetRates(action);
+                result = (float)Math.Round(result, 2);
+            }
+            else if (MainWindow.FromOrTo == "to")
+            {
+                result = amountOfMoney / Exchange.GetRates(action);
+                result = (float)Math.Round(result, 2);
+            }
+            action = "";
+        }
+
+        void FindCurrency()
+        {
+            if (_mainWindow.fromUSD.IsChecked == true || _mainWindow.toUSD.IsChecked == true) { action += "usd"; }
+            else if (_mainWindow.fromEUR.IsChecked == true || _mainWindow.toEUR.IsChecked == true) { action += "eur"; }
+            else if (_mainWindow.fromRUB.IsChecked == true || _mainWindow.toRUB.IsChecked == true) { action += "rub"; }
+            else if (_mainWindow.fromUAH.IsChecked == true || _mainWindow.toUAH.IsChecked == true) { action += "uah"; }
+            else if (_mainWindow.fromMDL.IsChecked == true || _mainWindow.toMDL.IsChecked == true) { action += "mdl"; }
+        }
+        void SellOrBuy()
+        {
+            if (_mainWindow.SellButton.IsChecked == true)
+            {
+                action += "buy";
+            }
+            else if (_mainWindow.BuyButton.IsChecked == true)
+            {
+                action += "sell";
+            }
+        }
+
+        public void HuesosOrNo()
+        {
+            bool huesos1 = true;
+            bool huesos2 = true;
+            foreach (var item1 in _mainWindow.ConversionPanel.Children)
+            {
+                if (item1 is RadioButton button1)
+                    if (button1.IsChecked == true)
+                    { huesos1 = false; break; }
+            }
+            foreach (var item2 in _mainWindow.SellOrBuyPanel.Children)
+            {
+                if (item2 is RadioButton button2)
+                    if (button2.IsChecked == true)
+                    { huesos2 = false; break; }
+            }
+            if (!huesos1 && !huesos2) huesos = false;
+        }
+    }
+}
